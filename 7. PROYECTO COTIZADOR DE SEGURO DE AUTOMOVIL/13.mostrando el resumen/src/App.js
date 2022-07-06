6a973f774d9237f5d9e734044ed45ea8dbea52c9
Julk16 <102524944+Julk16@users.noreply.github.com>
2022-07-05 19:56:05 -0500

    Add files via upload

---
+++ 7. PROYECTO COTIZADOR DE SEGURO DE AUTOMOVIL/13.mostrando el resumen/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+
+import Resumen from './components/Resumen'
+
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+ max-width: 600px;
+ margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+ background-color: #fff;
+ padding: 3rem;
+`
+
+function app() {
+ const [resumen, guardarResumen] = useState({
+  cotizacion: 0,
+  datos: {
+   marca: '',
+   year: '',
+   plan: '',
+  },
+ })
+
+ // extraer datos
+ const { datos } = resumen
+ return (
+  <contenedor>
+   <Header titulo='Cotizador de Seguros' />
+
+   <contenedorformulario>
+    <Formulario guardarResumen={guardarResumen} />
+
+    <resumen datos={datos} />
+   </contenedorformulario>
+  </contenedor>
+ )
+}
+
+export default app

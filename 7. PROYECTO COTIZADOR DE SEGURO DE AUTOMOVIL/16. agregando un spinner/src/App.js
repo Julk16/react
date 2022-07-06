e864ccb18711880b3c3bb088338227f3b49c1fd6
Julk16 <102524944+Julk16@users.noreply.github.com>
2022-07-05 19:57:53 -0500

    Add files via upload

---
+++ 7. PROYECTO COTIZADOR DE SEGURO DE AUTOMOVIL/16. agregando un spinner/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+
+import Resumen from './components/Resumen'
+
+import Resultado from './components/Resultado'
+
+import Spinner from './components/Spinner'
+
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+ max-width: 600px;
+ margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+ background-color: #fff;
+ padding: 3rem;
+`
+
+function app() {
+ const [resumen, guardarResumen] = useState({
+  cotizacion: 0,
+  datos: {
+   marca: '',
+   year: '',
+   plan: '',
+  },
+ })
+
+ const [cargando, guardarCargando] = useState(false)
+
+ // extraer datos
+ const { cotizacion, datos } = resumen
+ return (
+  <contenedor>
+   <Header titulo='Cotizador de Seguros' />
+
+   <contenedorformulario>
+    <Formulario
+     guardarResumen={guardarResumen}
+     guardarCargando={guardarCargando}
+    />
+
+    {cargando ? <Spinner /> : null}
+
+    <resumen datos={datos} />
+
+    {!cargando ? <Resultado cotizacion={cotizacion} /> : null}
+   </contenedorformulario>
+  </contenedor>
+ )
+}
+
+export default app

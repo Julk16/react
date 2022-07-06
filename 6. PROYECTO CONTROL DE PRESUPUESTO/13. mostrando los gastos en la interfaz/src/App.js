0a504695ac8481a63c3afd6ddd1b028b651f4f12
Julk16 <102524944+Julk16@users.noreply.github.com>
2022-07-05 19:47:01 -0500

    Add files via upload

---
+++ 6. PROYECTO CONTROL DE PRESUPUESTO/13. mostrando los gastos en la interfaz/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import pregunta from './components/pregunta'
+import Formulario from './components/Formulario'
+import Listado from './components/Listado'
+
+function app() {
+ // definir el state
+ const [presupuesto, guardarPresupuesto] = useState(0)
+ const [restante, guardarRestante] = useState(0)
+ const [mostrarpregunta, actualizarPregunta] = useState(true)
+ const [gastos, guardarGastos] = useState([])
+
+ //cuando agreguemos un nuevo gasto
+ const agregarnuevogasto = (gasto) => {
+  guardarGastos([...gastos, gasto])
+ }
+
+ return (
+  <div className='container'>
+   <header>
+    <h1>Gasto Semanal</h1>
+
+    <div className='contenido-principal contenido'>
+     {mostrarpregunta ? (
+      <Pregunta
+       guardarPresupuesto={guardarPresupuesto}
+       guardarRestante={guardarRestante}
+       actualizarPregunta={actualizarPregunta}
+      />
+     ) : (
+      <div className='row'>
+       <div className='one-half column'>
+        <Formulario agregarnuevoGasto={agregarnuevoGasto} />
+       </div>
+
+       <div className='one-half column'>
+        <Listado gastos={gastos} />
+       </div>
+      </div>
+     )}
+    </div>
+   </header>
+  </div>
+ )
+}
+
+export default App
---
+++ 6. PROYECTO CONTROL DE PRESUPUESTO/14. creando el componente que mostrara el presupuesto y el restante/src/components/listado.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Listado = ({ gastos }) => (
+ <div className='gastos-realizados'>
+  <h2>Listado</h2>
+  {gastos.map((gasto) => (
+   <Gasto key={gasto.id} gasto={gasto} />
+  ))}
+ </div>
+)
+
+export default listado
